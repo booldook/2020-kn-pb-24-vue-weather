df5bf69f17acfa5d8ee0e29945be797e0145ac68
booldook <booldook@gmail.com>
2020-08-20 13:13:05 +0900

    App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,27 @@
 <template lang="pug">
-	#app.container
+	#app.container.d-flex.flex-column.justify-content-between
 		nav-bar
+		router-view.wrapper
+		footer-bar
 </template>
 
 <script>
 import NavBar from './components/NavBar.vue'
+import Footer from './components/Footer.vue'
 
 export default {
 	name: 'App',
 	components: {
-		'nav-bar': NavBar
+		'nav-bar': NavBar,
+		'footer-bar': Footer,
 	}
 }
 </script>
 
 <style lang="less">
-
+div, ul, li, p, h1, h2, h3, h4, h5, form {margin: 0; padding: 0;}
+ul {list-style: none;}
+#app {height: 100vh; border-left: 1px solid #f1f1f1; border-right: 1px solid #f1f1f1;}
+.container {padding: 0 !important;}
+.wrapper {height: 100vh;}
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+	.footer-wrapper.bg-dark.text-light
+		.p-3.text-center &copy; Booldook
+</template>
+
+<script>
+export default {
+	name: 'Footer'
+}
+</script>
+
+<style scoped lang="less">
+
+</style>
