a55a01062469e7a5e5fcb295a4e17961fa10eabd
booldook <booldook@gmail.com>
2020-08-21 11:06:45 +0900

    Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,9 @@
 <template lang="pug">
 	.home-wrapper
 		.jumbotron
-			h1 일별/주별 날씨정보
-				small vue.js로 개발한 이용한 날씨정보 앱
+			h1.text-primary 일별/주별 날씨정보
+				p.pl-3.text-secondary
+					small vue.js로 개발한 이용한 날씨정보 앱
 		city-list
 </template>
 
@@ -17,6 +18,16 @@ export default {
 }
 </script>
 
-<style lang="less">
-
+<style scoped lang="less">
+@767: ~"(max-width: 767px)";
+@575: ~"(max-width: 575px)";
+.jumbotron {
+	background-color: #efefef;
+	@media @767 {
+		h1 {font-size: 2rem;}
+	}
+	@media @575 {
+		h1 {font-size: 1.5rem;}
+	}
+}
 </style>
