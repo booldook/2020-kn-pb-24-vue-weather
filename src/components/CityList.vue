c37a903ddc590a63733d16fc91b8a97de02b56d8
booldook <booldook@gmail.com>
2020-08-21 10:55:42 +0900

    Home

---
+++ src/components/CityList.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+	.city-wrapper
+		select.form-control(v-model='city' name='city' id='city')
+			option(value='날씨정보를 살펴볼 도시를 선택하세요.')
+</template>
+
+<script>
+export default {
+	name: 'CityList',
+	data() {
+		return {
+			city: ''
+		}
+	}
+}
+</script>
+
+<style lang="less">
+.city-wrapper {width: 90%; max-width: 600px; margin: 5rem auto 2rem auto;}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,19 @@
 <template lang="pug">
-	h1 HOME
+	.home-wrapper
+		.jumbotron
+			h1 일별/주별 날씨정보
+				small vue.js로 개발한 이용한 날씨정보 앱
+		city-list
 </template>
 
 <script>
+import CityList from '../components/CityList.vue'
+
 export default {
-	name: 'Home'
+	name: 'Home',
+	components: {
+		'city-list': CityList,
+	}
 }
 </script>
 
